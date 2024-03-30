ecad7d67ecb77cd6e391293eecadee9dc26d7072
impati <yongs170@naver.com>
2024-03-30 15:39:19 +0900

    first commit

---
+++ src/main/java/com/example/authorizationplatform/AuthorizationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.authorizationplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/authorizationplatform/AuthorizationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authorizationplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
